68a3cf6f630093dcdc3601663dcbd83137da03e0
Trung <89704613+trungpsu1210@users.noreply.github.com>
2022-06-14 10:13:51 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,43 +6,6 @@ from numpy import *
 import matplotlib.pyplot as plt
 import itertools
 
-def Draw_confusion_matrix(cm, target_names, title='Confusion matrix', cmap=None, normalize=True):
-    accuracy = 100*(np.trace(cm) / float(np.sum(cm)))
-    misclass = 1 - accuracy
-
-    if cmap is None:
-        cmap = plt.get_cmap('Blues')
-
-    plt.figure(figsize=(8, 6))
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-
-    if target_names is not None:
-        tick_marks = np.arange(len(target_names))
-        plt.xticks(tick_marks, target_names, rotation=45)
-        plt.yticks(tick_marks, target_names, rotation =0)
-
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        if normalize:
-            plt.text(j, i, "{:0.3f}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-        else:
-            plt.text(j, i, "{:,}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-
-    plt.tight_layout()
-    plt.ylabel('True label')
-    # plt.xlabel('Predicted label\nAccuracy={:0.3f}; Misclass={:0.3f}'.format(accuracy, misclass))
-    plt.xlabel('Predicted label')
-    plt.show()
-
 def convert_to_0_1_scale(array):
     new_array = np.zeros((2, 2))
     new_array[0] = array[0]/array[0].sum()
