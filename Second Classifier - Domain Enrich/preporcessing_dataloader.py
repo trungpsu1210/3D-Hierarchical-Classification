1fe020bff5c607b14fe15d5a6008b547e08ffdfa
Trung <89704613+trungpsu1210@users.noreply.github.com>
2022-06-14 11:00:01 -0400

    Update and rename Preporcessing_fromH5.py to preporcessing_dataloader.py

--- Second Classifier - Domain Enrich/preporcessing_dataloader.py
+++ Second Classifier - Domain Enrich/preporcessing_dataloader.py
@@ -10,21 +10,16 @@ import torch
 import argparse
 from torch.utils.data import DataLoader
 
-parser = argparse.ArgumentParser(description='Preprocessing data for model')
-
-parser.add_argument("--path_read_h5",type=str, default = './H5/Low - No Aug/Train_20_pct_46_samples.h5')
-
-opt = parser.parse_args()
-path_read_h5 = opt.path_read_h5
+parser = argparse.ArgumentParser(description='Preprocessing for dataloader')
 
 class DatasetFromHdf5():
   def __init__(self, file_path):
     super(DatasetFromHdf5, self).__init__()
     hf = h5py.File(file_path, 'r')
-    self.data_multislice = hf.get('dataset_multislice') # F x S x C x H x W
+    self.data_multislice = hf.get('dataset_multislice')
     self.data_alongtrack = hf.get('dataset_alongtrack')
     self.data_crosstrack = hf.get('dataset_crosstrack')
-    self.data_label = hf.get('dataset_label') # (F, 1)
+    self.data_label = hf.get('dataset_label')
     self.num_subvolume = self.data_multislice.shape[0]
     self.num_img_per_subvolume = self.data_multislice.shape[1]
 
@@ -42,11 +37,3 @@ class DatasetFromHdf5():
 
   def __len__(self):
     return self.data_multislice.shape[0]
-
-
-# data = DatasetFromHdf5(path_read_h5)
-# print(sum(data.data_label))
-# print(data.data_label.shape)
-# print(data.data_multislice.shape)
-# print(data.data_alongtrack.shape)
-# print(data.data_crosstrack.shape)
